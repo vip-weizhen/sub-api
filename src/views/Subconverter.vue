46a962f12abf0e485d0370fe2fc6fdb7d4473320
weizhen <47912513+vip-weizhen@users.noreply.github.com>
2025-06-30 15:21:53 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "Mr.WEI本站提供":"https://subconverter.weizhen.xyz/",
+          "Mr.WEI本站提供":"https://subconverter.weizhen.xyz",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
